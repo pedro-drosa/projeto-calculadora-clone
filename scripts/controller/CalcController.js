29c6b86e66f41cd285f57cf51bc39a0c273e4913
Pedro Juraci <pedro_drosa@hotmail.com>
2019-08-14 16:04:11 -0300

    new features and improvements

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController
 {
     constructor()
     {
+        this._lastOperator = "";
+        this._lastNumber = "";
         this._operation = [];
         this._locale = "pt-BR";
         this._displayCalcEl = document.querySelector('#display');
@@ -65,13 +67,35 @@ class CalcController
             // console.log(this._operation);
         }
     }
+    //retorna o valor da operação
+    getResult()
+    {
+        //console.log('getResult',this._operation);
+        return eval(this._operation.join(''));
+    }
     calc()
     {
         let last = "";
-        if(this._operation.length > 3){
-            last = this._operation.pop();
+
+        this._lastOperator =this.getLastItem(true);
+
+        if (this._operation.length < 3) {
+            let firstItem =  this._operation[0];
+            this._operation = [firstItem,this._lastOperator,this._lastNumber];
         }
-        let result  = eval(this._operation.join(''));
+        if(this._operation.length > 3){
+
+            last = this._operation.pop();
+            this._lastNumber = this.getResult();
+
+        }else if (this._operation.length == 3) {
+
+            this._lastNumber = this.getLastItem(false);
+        }
+        //console.log('lastOperator',this._lastOperator);
+        //console.log('lastNumber',this._lastNumber);
+        let result  = this.getResult();
+
         if(last == "%"){
             result /= 100;
             this._operation = [result];
@@ -82,16 +106,26 @@ class CalcController
         //atualizar display
         this.setLastNumberToDisplay();
     }
-    setLastNumberToDisplay()
+    getLastItem(isOperator = true)
     {
-        let lastNumber;
+        let lastItem;
         for (let i = this._operation.length-1; i >= 0; i--) {
-            if(!this.isOperator(this._operation[i])){
-                lastNumber = this._operation[i];
+            if(this.isOperator(this._operation[i]) == isOperator){
+                lastItem = this._operation[i];
                 break;
             }
         }
+        if (!lastItem) {
+            lastItem = (isOperator) ? this._lastOperator : this._lastNumber;
+        }
+        return lastItem;
+    }
+    setLastNumberToDisplay()
+    {
+        let lastNumber = this.getLastItem(false);
+        
         if(!lastNumber) lastNumber = 0;
+
         this.displayCalc = lastNumber;
     }
     addOperation(value)
